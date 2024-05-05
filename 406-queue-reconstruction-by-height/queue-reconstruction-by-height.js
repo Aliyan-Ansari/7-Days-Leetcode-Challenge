5b7d7fad524e3918d8db4acc7a8e7569815d7819
Aliyan <81861676+Aliyan-Ansari@users.noreply.github.com>
2024-05-06 01:31:54 +0500

    Time: 80 ms (82.65%) | Memory: 56.2 MB (78.57%) - LeetSync

---
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} people
+ * @return {number[][]}
+ */
+var reconstructQueue = function(people) {
+    people.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1];
+        } else {
+            return b[0] - a[0];
+        }
+    });
+
+    const result = [];
+
+    for (const person of people) {
+        result.splice(person[1], 0, person);
+    }
+
+    return result;
+};
