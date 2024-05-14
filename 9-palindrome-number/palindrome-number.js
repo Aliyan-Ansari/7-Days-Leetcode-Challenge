9ed275f07546d1df2c7fc3172109f6783a1a3933
Aliyan <81861676+Aliyan-Ansari@users.noreply.github.com>
2024-05-14 12:00:02 +0500

    Time: 113 ms (95.21%) | Memory: 56.3 MB (93.19%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -5,10 +5,6 @@
 var isPalindrome = function (x) {
     const data = x.toString();
     for (let i = 0; i < data.length / 2; i++) {
-
-        if (i === ((data.length - 1) - i)) {
-            return true;
-        }
         if (data.charAt(i) !== data.charAt((data.length - 1) - i)) {
             return false;
         }
